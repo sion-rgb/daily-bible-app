d680938ce13f5a4323f4237475c15c92e73c044e
sion-rgb <sion@sion-ng.com>
2026-06-19 12:14:17 +0800

    Add files via upload

---
+++ docs/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'daily-bible-v2.0.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './favicon.ico',
+  './site.webmanifest',
+  './data/bible-data.js',
+  './data/devotions-data.js',
+  './icons/favicon-16x16.png',
+  './icons/favicon-32x32.png',
+  './icons/apple-touch-icon.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
